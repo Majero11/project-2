bb0c0ec92bced5f5ad3475f183883ca00202cead
Tinmaj <majerographics@gmail.com>
2026-02-03 12:36:53 +0000

    added the java script code to the application to get the images for the API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,32 @@
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.search-button');
+const recipeContainer = document.querySelector('.recipe-container');
 
-const url = 'www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
+
+async function fetchData (input){
+    try{
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+        const recipies = await response.json();
+        // return recipies
+        recipies.meals.forEach(meal => {
+            const recipeDiv = document.createElement('div')
+            recipeDiv.classList.add('recipe');
+            recipeDiv.innerHTML = `
+                <img src="${meal.strMealThumb}">`
+
+            
+            recipeContainer.appendChild(recipeDiv);  
+        });
+    } catch(e){
+        console.log(e)
+    }
+}
+
+
+searchBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const recipeSearch = searchBox.value.trim()
+    fetchData(recipeSearch);
+    // console.log("button clicked")
+
+})
