b912a3047fc037732583ac80586f846218b53c0d
Tinmaj <majerographics@gmail.com>
2026-02-04 00:24:23 +0000

    added the recipe to the page using javascript. also did a bit of styling to the recipes as they are fetch to the page.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,26 +7,35 @@ async function fetchData (input){
     try{
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
         const recipies = await response.json();
-        // return recipies
-        recipies.meals.forEach(meal => {
-            const recipeDiv = document.createElement('div')
-            recipeDiv.classList.add('recipe');
-            recipeDiv.innerHTML = `
-                <img src="${meal.strMealThumb}">`
-
-            
-            recipeContainer.appendChild(recipeDiv);  
-        });
+        return recipies
     } catch(e){
         console.log(e)
     }
 }
 
+async function displayRecipe(){
+    const recipeSearch = searchBox.value.trim();
+    recipeContainer.innerHTML = "";
+
+    const recipe = await fetchData(recipeSearch)
+
+    recipe.meals.forEach(meal => {
+         const recipeDiv = document.createElement('div')
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML = `
+             <img src="${meal.strMealThumb}">
+             <div class="recipe-details">
+                 <h3 id="meal-name">${meal.strMeal}</h3>
+                 <p id="meal-detaisl">${meal.strArea} recipe | ${meal.strCategory}</p>
+             </div>
+             `
+        recipeContainer.appendChild(recipeDiv);  
+     });
+
+}
 
 searchBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    const recipeSearch = searchBox.value.trim()
-    fetchData(recipeSearch);
-    // console.log("button clicked")
+    displayRecipe();
 
 })
